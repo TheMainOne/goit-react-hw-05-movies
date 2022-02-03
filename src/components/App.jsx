591644926dbb9a643f718e92ff7e09c44530393f
Maksym Lvov <leva880@ukr.net>
2022-02-03 13:07:17 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-const KEY = '3f98aefed4f412e17fc5cbf8a78c872a';
+// const KEY = '3f98aefed4f412e17fc5cbf8a78c872a';
 
 export const App = () => {
   return <>React homework template</>;
