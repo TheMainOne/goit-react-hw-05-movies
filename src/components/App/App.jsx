15107218ab8be38985b38659f0ca9b918d7ba1b6
Maksym Lvov <leva880@ukr.net>
2022-02-11 16:12:15 +0300

    fix some problems

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,18 @@
+import React, { lazy, Suspense } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
 import { Nav, Link, Container, Header } from './App.styled';
 import GlobalStyle from './GlobalStyles';
 
-export const App = () => {
+const MoviesPage = lazy(() => import('../MoviesPage/MoviesPage'));
+const ItemPage = lazy(() => import('../pages/MovieDetailsPage'));
+const HomePage = lazy(() => import('../HomePage/HomePage'));
+const ActorsInfo = lazy(() => import('../pages/ActorsPage'));
+const ReviewsInfo = lazy(() => import('../pages/ReviewsPage'));
+
+const SiteHeader = () => {
   return (
     <Container>
-      <GlobalStyle />
       <Header>
         <Nav>
           <Link to="/">Home</Link>
@@ -17,3 +24,26 @@ export const App = () => {
     </Container>
   );
 };
+
+const App = () => {
+  return (
+    <>
+      <Suspense fallback="">
+        <Routes>
+          <Route path="/" element={<SiteHeader />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="movies" element={<MoviesPage />} />
+            <Route path="movies/:movieId" element={<ItemPage />}>
+              <Route path="cast" element={<ActorsInfo />} />
+              <Route path="reviews" element={<ReviewsInfo />} />
+            </Route>
+            <Route path="*" element={<Navigate to="/" />} />
+          </Route>
+        </Routes>
+      </Suspense>
+      <GlobalStyle />
+    </>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,34 @@
-import React, { lazy, Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { App } from 'components/App/App';
-
-const MoviesPage = lazy(() => import('./components/MoviesPage/MoviesPage'));
-const ItemPage = lazy(() => import('./components/pages/MovieDetailsPage'));
-const HomePage = lazy(() => import('./components/HomePage/HomePage'));
-const ActorsInfo = lazy(() => import('./components/pages/ActorsPage'));
-const ReviewsInfo = lazy(() => import('./components/pages/ReviewsPage'));
+import { BrowserRouter } from 'react-router-dom';
+import App from 'components/App/App';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Suspense fallback="">
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="movies" element={<MoviesPage />} />
-            <Route path="movies/:movieId" element={<ItemPage />}>
-              <Route path="cast" element={<ActorsInfo />} />
-              <Route path="reviews" element={<ReviewsInfo />} />
-            </Route>
-            <Route path="*" element={<Navigate to="/" />} />
-          </Route>
-        </Routes>
-      </Suspense>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+// <Suspense fallback="">/
+//         <Routes>
+//           <Route path="/" element={<App />}>
+//             <Route path="/" element={<HomePage />} />
+//             <Route path="movies" element={<MoviesPage />} />
+//             <Route path="movies/:movieId" element={<ItemPage />}>
+//               <Route path="cast" element={<ActorsInfo />} />
+//               <Route path="reviews" element={<ReviewsInfo />} />
+//             </Route>
+//             <Route path="*" element={<Navigate to="/" />} />
+//           </Route>
+//         </Routes>
+//       </Suspense>
+//     </BrowserRouter>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
