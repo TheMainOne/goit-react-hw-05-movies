8776724bee03538c13d2d04d2622ac9dc0309397
Maksym Lvov <leva880@ukr.net>
2022-02-23 19:17:39 +0300

    fixed problems with loading cast and review pages

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,12 +2,12 @@ import React, { lazy, Suspense } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import GlobalStyle from './GlobalStyles';
 import { SiteHeader } from 'components/SiteHeader/SiteHeader';
+import ActorsInfo from 'components/pages/ActorsPage';
+import ReviewsInfo from 'components/pages/ReviewsPage';
 
 const MoviesPage = lazy(() => import('../MoviesPage/MoviesPage'));
 const ItemPage = lazy(() => import('../pages/MovieDetailsPage'));
 const HomePage = lazy(() => import('../HomePage/HomePage'));
-const ActorsInfo = lazy(() => import('../pages/ActorsPage'));
-const ReviewsInfo = lazy(() => import('../pages/ReviewsPage'));
 
 const App = () => {
   return (
